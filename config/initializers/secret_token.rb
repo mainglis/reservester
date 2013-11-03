a23f1172afb2aea29dc769c608869b3b771feef0
Bob Breznak <bob.breznak@gmail.com>
2013-11-03 10:07:58 -0500

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = '0aa5fb3fcfac41da54539d4df4b4fe7b8c7671e1f070451fb4682e4610562b09fcccfeb62ac3594959f6f45c1d6fcaf2488a4e008b26d00abcaaa99554de6941'
